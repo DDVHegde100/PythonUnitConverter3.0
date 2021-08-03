623d627cc6950bef6295921c14e9f1f2770d75ea
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-03 10:20:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 kilometers=float(input('How many kilometers?:'))
 conv_fac=0.621371
 miles=kilometers*conv_fac
@@ -8,3 +7,8 @@ print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
 celsius=float(input('Enter temperature in Celsius: '))
 fahrenheit=(celsius * 1.8)+32
 print('%0.1f Celsius is equal to %0.1f degree Fahrenheit'%(celsius, fahrenheit))
+
+feet=float(input('Enter measurement in feet: '))
+meters=feet*0.3048
+print('%0.1f feet is equal to %0.1f meters'%(feet, meters))
+
